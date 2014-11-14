f1f1a44b78ea59ee14ce1c50f3d28ac51f609105
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-14 16:19:43 +0900

    add: example/exaple.lua has been added.

---
+++ example/example.lua
@@ -0,0 +1,31 @@
+local inspect = require('util').inspect;
+local HttpCli = require('httpcli.luasocket');
+
+local myDefaultTimeout = 60; -- seconds
+local uppercaseHeaderName = true; -- convert header names to uppercase
+local cli, err = HttpCli.new( myDefaultTimeout, uppercaseHeaderName );
+
+if err then
+    print( err );
+else
+    local timeoutForThisRequest = 30;
+    local res;
+    
+    res, err = cli:get( 'http://example.com/', { -- also, can be pass the https url
+        -- query = <table>,
+        -- header = <table>,
+        -- body = <string or table>
+        -- enctype = <'json' or 'form' or content-type string>,
+    }, timeoutForThisRequest );
+
+    -- response table has the following fields;
+    -- status: number (HTTP status code)
+    -- header: <table or nil>
+    -- body  : <string or table or nil>
+    --
+    -- NOTE-1: header and body will be nil if status is 408 408 Request Timeout
+    -- NOTE-2: if content-type header is a 'application/json' then decode a 
+    --         body strings on internally, and set an error strings to second 
+    --         returns value on failure.
+    print( inspect({ res, err }) );
+end
